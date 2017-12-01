ccc3f4dd0b8ddce4877a3eba4f2787ec1fd0a4b4
Your Name <you@exampgit config --global user.email you@example.com>
2017-12-01 11:02:08 -0600

    cleaned up files

--- tracker.py
+++ tracker.py
@@ -1,4 +1,5 @@
 from particle import TrackedParticle
+import numpy as np
 
 class TrackManager:
 
@@ -40,4 +41,52 @@ class TrackManager:
                 pass
 
         self.particle_list = temp_particle_list
+    
+    def match_particles(self, current_frame_raw, current_frame_feature, ASSUME_SAME_PARTICLE_DISTANCE = 50):
+	for current_frame_index in range(len(current_frame_raw)):
+	    smalled_distance = np.inf
+	    smalled_distance_current_frame_index = -1
+	    for prev_particle in self.get_particle_list():
+
+		# There is a eucledian distance between the center of the objects 
+		distance_eucledian = np.sqrt((prev_particle.get_predicted_x()[0]- 
+			    current_frame_raw[current_frame_index][0])**2 + 
+			    (prev_particle.get_predicted_x()[2] -
+			    current_frame_raw[current_frame_index][1])**2)[0]
+		
+		
+		distance_feature = np.sqrt((prev_particle.width - 
+		current_frame_feature[current_frame_index][0])**2 + 
+		(prev_particle.height - current_frame_feature[current_frame_index][0])**2)
+
+		distance_combined = np.sqrt((distance_eucledian)**2 + distance_feature**2)
+		    #Find out which particle is the closest to the prediction
+		if distance_eucledian < smalled_distance:
+		    smalled_distance_current_frame_index = current_frame_index
+		    smalled_distance = distance_combined
+
+	    # Update the prediction for the best particle match or create a new particle
+		#print ("Smallest: "+ str(smalled_distance))
+		if smalled_distance < ASSUME_SAME_PARTICLE_DISTANCE: 
+		    measurmet =current_frame_raw[smalled_distance_current_frame_index]
+		    feature =current_frame_feature[smalled_distance_current_frame_index]
+
+		    prev_particle.update_location([int(measurmet[0]),int(measurmet[1])])
+		    prev_particle.update_feature(feature)
+
+#		cv2.rectangle(frame,(int(measurmet[0])-5,int(measurmet[1])-5),(int(measurmet[0])+5,int(measurmet[1])+5),(0,0,255),2)
+		    break
+
+#	    print ("Measurment location raw:" + str(current_frame_raw[smalled_distance_current_frame_index]))
+#	    print ("Measurment location Best:" + str(prev_particle.get_predicted_x()))
+#           print ("Matched id: "+ str(prev_particle.trackID)+" to "+str(smalled_distance_current_frame_index))
+		#text_position = [int(prev_particle.get_predicted_x()[0]),int(prev_particle.get_predicted_x()[2])]
+		#cv2.rectangle(frame,(int(text_position[0]),int(text_position[1])),(text_position[0]+10,text_position[1]+10),(255,0,0),2)
+		#print ("Kalman Output: "+ str(prev_particle.get_predicted_x()[0])+ ","+str(prev_particle.get_predicted_x()[2]))
+		#cv2.putText(frame,str(prev_particle.trackID),(int(text_position[0]),int(text_position[1])), font,fontScale,fontColor, lineType)
+	    else:
+		new_particle_id = self.new_particle(current_frame_raw[smalled_distance_current_frame_index], current_frame_feature[smalled_distance_current_frame_index])
+		print ("Added new particle with ID:"+str(new_particle_id))
+
+
 
